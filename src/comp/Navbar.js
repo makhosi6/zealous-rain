fb79137e166c77d94c7a1abd8c86b370ba8c3832
makhosi6 <ndondo330@gmail.com>
2021-06-17 20:58:46 +0200

    Thursday 06/17/2021 20:58 - Updates

--- src/comp/Navbar.js
+++ src/comp/Navbar.js
@@ -1,4 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {AppBar, Toolbar, Typography,Button, IconButton} from '@material-ui/core';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 export default class Navbar extends Component {
     render() {
