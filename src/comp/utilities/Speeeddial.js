e8d3eee021d4ffcc9a11a05dbc8839ccc9b2502c
makhosi6 <ndondo330@gmail.com>
2021-06-17 20:58:46 +0200

    Thursday 06/17/2021 20:58 - Updates

--- src/comp/utilities/Speeeddial.js
+++ src/comp/utilities/Speeeddial.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Fab from "@material-ui/core/Fab";
 import Mail from "@material-ui/icons/Mail";
+import { Link } from 'react-router-dom';
 
 export default class Speeeddial extends Component {
   render() {
