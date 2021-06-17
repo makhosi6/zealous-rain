683eb4757cfbd114a60e50d964bb972cd54577a1
makhosi6 <ndondo330@gmail.com>
2021-06-17 20:04:15 +0200

    Thursday 06/17/2021 20:04 - Loader, Error Page

--- src/App.js
+++ src/App.js
@@ -2,40 +2,53 @@ import React from "react";
 import Home from "../src/pages/Home";
 import Error from "../src/pages/Error";
 import { Switch, Route } from "react-router-dom";
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline } from "@material-ui/core";
 import Messages from "./pages/Messages";
 import Navbar from "./comp/Navbar";
+import Loader from "./comp/utilities/Loader";
 
 export default class App extends React.Component {
- state = {
-       error: "",
-  }
+  state = {
+    error: "",
+    hasLoaded: false,
+  };
   componentDidCatch(error, info) {
     // Display fallback UI
-    console.log({error})
+    console.log({ error });
     this.setState({ hasError: true });
     this.setState({ error: error.message });
   }
- 
-render() {
-  if (this.state.hasError) {
-    // You can render any custom fallback UI
-    return (
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ hasLoaded: true });
+    }, 3500);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // This is a fallback UI
+      return (
         <>
-          <Error error={this.state.error}/>
+          <Error error={this.state.error} />
         </>
+      );
+    }
+    return (
+      //https://material-ui.com/components/snackbars/
+      <React.Fragment>
+        <CssBaseline />
+        {this.state.hasLoaded ? (
+          <>
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/messages" component={Messages} />
+            </Switch>
+          </>
+        ) : (
+          <Loader />
+        )}
+      </React.Fragment>
     );
   }
-  return (
-    //https://material-ui.com/components/snackbars/
-    <React.Fragment>
-    <CssBaseline />
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/messages" component={Messages} />
-      </Switch>
-      </React.Fragment>
-  );
-}}
-
+}
