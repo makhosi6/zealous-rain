96d05d71f95461c00d53946a2e2b49d14e5a742a
makhosi6 <ndondo330@gmail.com>
2021-06-18 19:13:53 +0200

    Friday 06/18/2021 19:13 - Updates

--- src/comp/Message.js
+++ src/comp/Message.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
+import {ListItemText,ListItem, Divider, ListItemAvatar, Avatar }from "@material-ui/core/ListItemText";
 import ImageIcon from "@material-ui/icons/Image";
 
 export default class Message extends Component {
