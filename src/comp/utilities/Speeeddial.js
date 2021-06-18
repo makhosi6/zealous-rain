a7fd47a47b859de830d61f303fcaa27299cf12b2
makhosi6 <ndondo330@gmail.com>
2021-06-18 18:43:16 +0200

    Friday 06/18/2021 18:43 - Mssgs

--- src/comp/utilities/Speeeddial.js
+++ src/comp/utilities/Speeeddial.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import Fab from "@material-ui/core/Fab";
 import Mail from "@material-ui/icons/Mail";
-import { Link } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+
 
 export default class Speeeddial extends Component {
   render() {
@@ -9,10 +10,10 @@ export default class Speeeddial extends Component {
       <Fab
         variant="extended"
         style={{ position: "absolute", bottom: "20px", right: "20px" }} >
-        {/* <Link to="/messages"> */}
+        <Link href="/messages" color="inherit">
           <Mail style={{marginRight: "10px"}}/>
           Messages
-        {/* </Link> */}
+        </Link>
       </Fab>
     );
   }
--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,48 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Container } from '@material-ui/core';
 
 export default class Messages extends Component {
-    render() {
-        return (
-            <>
-               {
-                   mssg.map(x => <li>{x.message}</li>)
-               }
-            </>
-        )
-    }
+  render() {
+    return (
+      <Container maxWidth="md">
+        <ol>
+          {mssgs.map((x) => (
+            <li>{x.message}</li>
+          ))}
+        </ol>
+      </Container>
+    );
+  }
 }
 
-
-
-let mssg = [
-    {
-        "message": "Engine for CEO12915 is ON and the taxi is about to move  at  2 Worcester Street Grabouw Grabouw",
-        "subject": "Engine ON for CEO12915",
-        "display": "15-Jun"
-      },
-      {
-        "message": "Engine for CF247139 is OFF and the taxi is parked  at  Castle 1 Foreshore Cape Town Cape Town",
-        "subject": "Engine OFF for CF247139",
-        "display": "15-Jun"
-      },
-      {
-        "message": "Engine for CF247139 is ON and the taxi is about to move  at  Castle 1 Foreshore Cape Town Cape Town",
-        "subject": "Engine ON for CF247139",
-        "display": "15-Jun"
-      },
-      {
-        "message": "Engine for CF66306 is OFF and the taxi is parked  at  Kraaifontein East Cape Town Cape Town Western Cape",
-        "subject": "Engine OFF for CF66306",
-        "display": "15-Jun"
-      },
-      {
-        "message": "Engine for CF66306 is ON and the taxi is about to move  at  Kraaifontein East Cape Town Cape Town Western Cape",
-        "subject": "Engine ON for CF66306",
-        "display": "15-Jun"
-      },
-      {
-        "message": "Engine for CF66306 is OFF and the taxi is parked  at  Kraaifontein East Cape Town Cape Town Western Cape",
-        "subject": "Engine OFF for CF66306",
-        "display": "15-Jun"
-      }
-]
+let mssgs = [
+  {
+    message:
+      "Engine for CEO12915 is ON and the taxi is about to move  at  2 Worcester Street Grabouw Grabouw",
+    subject: "Engine ON for CEO12915",
+    display: "15-Jun",
+  },
+  {
+    message:
+      "Engine for CF247139 is OFF and the taxi is parked  at  Castle 1 Foreshore Cape Town Cape Town",
+    subject: "Engine OFF for CF247139",
+    display: "15-Jun",
+  },
+  {
+    message:
+      "Engine for CF247139 is ON and the taxi is about to move  at  Castle 1 Foreshore Cape Town Cape Town",
+    subject: "Engine ON for CF247139",
+    display: "15-Jun",
+  },
+  {
+    message:
+      "Engine for CF66306 is OFF and the taxi is parked  at  Kraaifontein East Cape Town Cape Town Western Cape",
+    subject: "Engine OFF for CF66306",
+    display: "15-Jun",
+  },
+  {
+    message:
+      "Engine for CF66306 is ON and the taxi is about to move  at  Kraaifontein East Cape Town Cape Town Western Cape",
+    subject: "Engine ON for CF66306",
+    display: "15-Jun",
+  },
+  {
+    message:
+      "Engine for CF66306 is OFF and the taxi is parked  at  Kraaifontein East Cape Town Cape Town Western Cape",
+    subject: "Engine OFF for CF66306",
+    display: "15-Jun",
+  },
+];
