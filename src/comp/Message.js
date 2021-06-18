0d4a2b1e54838cfd3c9d652516cff4f949a70cf0
makhosi6 <ndondo330@gmail.com>
2021-06-18 18:49:04 +0200

    Friday 06/18/2021 18:49 - Mssg Strip/

--- src/comp/Message.js
+++ src/comp/Message.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -7,19 +6,12 @@ import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import ImageIcon from "@material-ui/icons/Image";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
 
 export default class Message extends Component {
   render() {
-    const classes = useStyles();
     return (
-      <List className={classes.root}>
+      <List style={{ width: "100%",
+      maxWidth: "400px",}}>
         <ListItem>
           <ListItemAvatar>
             <Avatar>
