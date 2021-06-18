4410b00454c2b2a7428f33417c6e61f79c67225f
makhosi6 <ndondo330@gmail.com>
2021-06-18 22:09:21 +0200

    Friday 06/18/2021 22:09 - Updates, Map, Packages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,14 @@
 import React, { Component } from "react";
 import Speaddial from "../comp/utilities/Speeeddial";
 import { Container } from "@material-ui/core";
+import Map from "../comp/Map";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <Container>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13243.112263032506!2d18.4288102!3d-33.9211111!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x607472ea9e6a0724!2sCape%20Town%20Civic%20Centre!5e0!3m2!1sen!2sza!4v1624042173435!5m2!1sen!2sza"
-           
-            style={{
-                width: "100vw",
-                height: "95vh",
-              border: "0",
-            }}
-            allowfullscreen
-            loading="lazy"
-          >
-            {" "}
-          </iframe>{" "}
+          <Map/>
           <Speaddial />
         </Container>{" "}
       </div>
