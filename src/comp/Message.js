6f6f09e34ab9d8c63413d0cfe7e366e8f6d7920f
makhosi6 <ndondo330@gmail.com>
2021-06-18 18:49:04 +0200

    Friday 06/18/2021 18:49 - Mssg Strip/

--- src/comp/Message.js
+++ src/comp/Message.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -6,19 +6,22 @@ import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import ImageIcon from "@material-ui/icons/Image";
 
-
 export default class Message extends Component {
   render() {
     return (
-      <List style={{ width: "100%",
-      maxWidth: "400px",}}>
+      <List style={{ width: "100%", maxWidth: "400px" }}>
         <ListItem>
           <ListItemAvatar>
             <Avatar>
               <ImageIcon />
             </Avatar>
           </ListItemAvatar>
-          <ListItemText primary={this.props.subject} secondary={this.props.message} />
+          <ListItemText
+            primary={this.props.subject}
+            secondary={this.props.message}
+          />
+          <span> 01-21 </span>
+          <span>*</span>
         </ListItem>
       </List>
     );
